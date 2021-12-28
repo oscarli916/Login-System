84a2d7e44763fab3d9298a7455904427166e0cb4
Oscar Li <oscar_li916@yahoo.com.hk>
2021-12-28 11:40:14 +0800

    style: golang auto styling

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	jwt "github.com/golang-jwt/jwt"
-	"example.com/loginSystem/login"
-	"example.com/loginSystem/header"
-)
 
+	"example.com/loginSystem/header"
+	"example.com/loginSystem/login"
+	jwt "github.com/golang-jwt/jwt"
+)
 
 var googleSigningKey = []byte("googlesigningkey")
 var facebookSigningKey = []byte("facebooksigningkey")
@@ -14,13 +14,11 @@ var appleSigningKey = []byte("applesigningkey")
 var OTPSigningKey = []byte("OTPsigningkey")
 var errorSigningKey = []byte("errorsigningkey")
 
-
-
 func generateToken(signingKey *[]byte) (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
 
 	tokenString, err := token.SignedString(*signingKey)
-	if err != nil{
+	if err != nil {
 		fmt.Printf("Something went wrong: %v", err)
 		return "", err
 	}
@@ -28,46 +26,45 @@ func generateToken(signingKey *[]byte) (string, error) {
 	return tokenString, nil
 }
 
-
-func generateHeaders() ([]header.Header){
+func generateHeaders() []header.Header {
 	headers := []header.Header{}
 
 	googleToken, err := generateToken(&googleSigningKey)
-	if err != nil{
+	if err != nil {
 		fmt.Printf("Error in generating Google Token.")
-	} else{
+	} else {
 		googleHeader := header.Header{Authorization: googleToken, Social: login.GOOGLE}
 		headers = append(headers, googleHeader)
 	}
 
 	facebookToken, err := generateToken(&facebookSigningKey)
-	if err != nil{
+	if err != nil {
 		fmt.Printf("Error in generating Facebook Token.")
-	} else{
+	} else {
 		facebookHeader := header.Header{Authorization: facebookToken, Social: login.FACEBOOK}
 		headers = append(headers, facebookHeader)
 	}
 
 	appleToken, err := generateToken(&appleSigningKey)
-	if err != nil{
+	if err != nil {
 		fmt.Printf("Error in generating Apple Token.")
-	} else{
+	} else {
 		appleHeader := header.Header{Authorization: appleToken, Social: login.APPLE}
 		headers = append(headers, appleHeader)
 	}
 
 	OTPToken, err := generateToken(&OTPSigningKey)
-	if err != nil{
+	if err != nil {
 		fmt.Printf("Error in generating OTP Token.")
-	} else{
+	} else {
 		OTPHeader := header.Header{Authorization: OTPToken, Social: login.OTP}
 		headers = append(headers, OTPHeader)
 	}
 
 	errorToken, err := generateToken(&errorSigningKey)
-	if err != nil{
+	if err != nil {
 		fmt.Printf("Error in generating Error Token.")
-	} else{
+	} else {
 		errorHeader := header.Header{Authorization: errorToken, Social: login.ERROR}
 		headers = append(headers, errorHeader)
 	}
@@ -76,16 +73,15 @@ func generateHeaders() ([]header.Header){
 
 }
 
-
-func main(){
+func main() {
 
 	headers := generateHeaders()
 
-	for _, header := range headers{
+	for _, header := range headers {
 		login, err := login.CreateLogin(header)
-		if (err != nil){
+		if err != nil {
 			fmt.Printf("Error in creating Login: %s \n\n", err)
-		} else{
+		} else {
 			login.Login()
 			login.GetUserData()
 		}
